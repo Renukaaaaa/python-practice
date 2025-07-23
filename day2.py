3b996a052826f0fb99b309e1ce30b2bde94f13de
renu <renupatil54321@gmail.com>
2025-07-23 20:01:07 +0530

    first commit

---
+++ day2.py
@@ -0,0 +1,19 @@
+print("Hello")
+print("Welcome to Python Programming...")
+print("Python is High level and Interpreted Language")
+print("Python is developed by Guido van rossum")
+print("Lets start coding..!")
+
+def student():
+    name="renuka"
+    age=21
+student()
+
+print("Single line comment")        #This is single line comment
+'''Hello welcome to python world     #this is multi line comment
+'''
+#to run file from terminal use: python filename.py
+
+def addition():
+    print("addition is=",3+6)
+addition()
---
+++ day3.py
@@ -0,0 +1,10 @@
+name=str(input("Enter the name:"))
+print("My name is",{name},type(name))
+
+age=int(input("Enter your age:"))
+print("My age is",{age},type(age))
+
+newint=int(name)
+print(newint,'newint',type(newint))
+new_float=float(age)
+print(new_float,type(new_float))
